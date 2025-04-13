efeda870cefe083e1cf25aef635da3f1c42f96b8
B3THMAK <132462124+B3THMAK@users.noreply.github.com>
2025-04-13 23:02:33 +0300

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ApplicantDashboard from './pages/ApplicantDashboard';
-
+import AdminLayout from './components/admin/AdminLayout';
+import ApplicantsList from './pages/admin/ApplicantsList';
+import ApplicantProfile from './pages/admin/ApplicantProfile';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/dashboard/*" element={<ApplicantDashboard />} />
+        <Route path="/admin" element={<AdminLayout><ApplicantsList /></AdminLayout>} />
+        <Route path="/admin/applicants" element={<AdminLayout><ApplicantsList /></AdminLayout>} />
+        <Route path="/admin/applicants/:id" element={<AdminLayout><ApplicantProfile /></AdminLayout>}
+/>
         {/* Add login/register and admin dashboard here */}
       </Routes>
     </Router>
--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,22 +1,8 @@
 import axios from 'axios';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AdminLayout from './components/admin/AdminLayout';
-import ApplicantsList from './pages/admin/ApplicantsList';
+
 const API = axios.create({
   baseURL: 'http://localhost:5000/api',
 });
-
-function App() {
-    return (
-      <Router>
-        <Routes>
-          <Route path="/admin" element={<AdminLayout><ApplicantsList /></AdminLayout>} />
-          <Route path="/admin/applicants" element={<AdminLayout><ApplicantsList /></AdminLayout>} />
-          {/* Add other admin routes here */}
-        </Routes>
-      </Router>
-    );
-  }
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
@@ -24,4 +10,4 @@ API.interceptors.request.use((config) => {
   return config;
 });
 
-export default App;
+export default API;
